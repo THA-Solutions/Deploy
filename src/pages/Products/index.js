6d6d7253d293e0ce8ce8f91acbd67c39929e40b6
THA Solutions <tecnologia@thasolutions.com.br>
2023-06-20 13:54:42 -0300

    Update index.js

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -33,7 +33,7 @@ export default function Products() {
 
     async function fetchData() {
       try {
-        const response = await axios.get("https://fgldistribuidora.vercel.app/api/product/getProductList", {
+        const response = await axios.get("fgldistribuidora.com.br/api/product/getProductList", {
           params: { divisao: value },
         });
 
