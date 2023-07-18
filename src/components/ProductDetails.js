868fbd42fd2cfa1e45f1f76b4c545dea69504dd1
Gean <geancarlostha@gmail.com>
2023-07-18 13:53:15 -0300

    18/07/2023

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -110,7 +110,8 @@ export default function ProductDetails(props) {
   // Manipula o texto para quebrar as linhas
   const manipulateText = (text) => {
     if (text) {
-      const lines = text.split("\n").replace(/^\/n\w+/i.test(key), "");
+      let lines = text.split("\n").map((line) => line.replace(regex, ""));
+
       const manipulatedLines = lines.map((line, index) => {
         return (
           <React.Fragment key={index}>
